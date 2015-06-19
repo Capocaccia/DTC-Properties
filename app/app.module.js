3aee4b70fdddec05066415fadbdf14a437d6ed52
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-06-18 19:59:04 -0500

    boilerplate

---
+++ app/app.module.js
@@ -0,0 +1,12 @@
+angular
+  .module("dtcModule", [])
+  // .config(function ($routeProvider) {
+  //   $routeProvider
+  //   .when('/', {
+  //     templateUrl: 'app/components/about/aboutView.html',
+  //     controller: 'AboutCtrl'
+  //     })
+  //   });
+
+
+//I know this .config is right.  Need a little guidance here.
---
+++ app/components/about/aboutController.js
@@ -0,0 +1,5 @@
+angular
+  .module('dtcModule')
+  .controller("aboutCtrl", function(){
+
+  });
